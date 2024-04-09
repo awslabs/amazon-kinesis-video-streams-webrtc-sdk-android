32523d3bec86a067f601c23fef4d1a6ae51fb359
niyatim23 <niyatim23@gmail.com>
2024-04-09 09:44:30 -0700

    cleanup KinesisVideoWebRtcDemoApp

--- app/src/main/java/com/amazonaws/kinesisvideo/demoapp/KinesisVideoWebRtcDemoApp.java
+++ app/src/main/java/com/amazonaws/kinesisvideo/demoapp/KinesisVideoWebRtcDemoApp.java
@@ -4,11 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.kinesisvideo.common.logging.LogLevel;
-import com.amazonaws.kinesisvideo.common.logging.OutputChannel;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.config.AWSConfiguration;
-import com.amazonaws.mobileconnectors.kinesisvideo.util.AndroidLogOutputChannel;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,9 +14,6 @@ public class KinesisVideoWebRtcDemoApp extends Application {
     private static final String TAG = KinesisVideoWebRtcDemoApp.class.getSimpleName();
 
     public static AWSCredentialsProvider getCredentialsProvider() {
-        final OutputChannel outputChannel = new AndroidLogOutputChannel();
-        final com.amazonaws.kinesisvideo.common.logging.Log log =
-                new com.amazonaws.kinesisvideo.common.logging.Log(outputChannel, LogLevel.VERBOSE, TAG);
         return AWSMobileClient.getInstance();
     }
 
