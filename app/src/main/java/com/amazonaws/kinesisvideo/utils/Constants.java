046e8f2ab13bd37bcf849187648749f292602f7c
Hassan Sahibzada <hsahibza@amazon.com>
2024-04-17 15:14:59 -0400

    upgrade gradle to resolve incompatible kotlin version errors, bump version for upcoming release

--- app/src/main/java/com/amazonaws/kinesisvideo/utils/Constants.java
+++ app/src/main/java/com/amazonaws/kinesisvideo/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     /**
      * SDK version identifier
      */
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     /**
      * Query parameter for Channel ARN. Used for calling Kinesis Video Websocket APIs.
