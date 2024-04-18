a8c9b6a1c58a6eafdce4a6c78d957fb0d469e84e
sirknightj <jggunawa@amazon.com>
2024-04-18 10:08:29 -0700

    Dynamic version constant (#109)
    * Dynamic version constant
    
    * Readd package

--- app/src/main/java/com/amazonaws/kinesisvideo/utils/Constants.java
+++ app/src/main/java/com/amazonaws/kinesisvideo/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.amazonaws.kinesisvideo.utils;
 
+import com.amazonaws.kinesisvideo.demoapp.BuildConfig;
+
 public class Constants {
     /**
      * SDK identifier
@@ -8,7 +10,7 @@ public class Constants {
     /**
      * SDK version identifier
      */
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = BuildConfig.VERSION_NAME;
 
     /**
      * Query parameter for Channel ARN. Used for calling Kinesis Video Websocket APIs.
